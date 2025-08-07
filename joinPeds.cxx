2d19e0ca3022e9a195dbf3d52dc16e7297b1de1f
piacenst <stefano.piacentini@hotmail.com>
2025-08-07 11:51:49 +0000

    refurbishing of joinPeds: same structure of main.cxx is used

--- joinPeds.cxx
+++ joinPeds.cxx
@@ -7,115 +7,65 @@
  *
  */
 
-#include <iostream>
-#include <unistd.h>
-#include <limits.h>
-#include <sstream>
-#include <fstream>
-#include <chrono>
-#include <map>
-#include <random>
-#include <string>
-//#include <execution>
-#include <numeric>
-#include <algorithm>
-#include <cmath>
-#include <stdio.h>
-#include <cstdlib>
-#include <filesystem>
-#include <memory>
-#include "TRandom3.h"
-#include "TFile.h"
-#include "TTree.h"
-#include "TH1.h"
-#include "TH2.h"
-#include "s3.h"
-#include "cygnolib.h"
-
-using namespace std;
-
-vector<string> split(const string& , char );
-void ReadConfig(string name, map<string,string>& options);
-
-int main(int argc, char** argv)
-{
-    
-    if(argc<2) {cerr<<"No Configfile given!!\nSuggested use: ./progname.exe Configfile -I inputdir -O outputdir"; exit(EXIT_FAILURE);}
-    string nome=argv[1];
-    
-    vector<string> path_to_config=split(nome,'/');
-    string parte= "";
-    for(unsigned int i=0;i<path_to_config.size()-2;i++) parte=parte+path_to_config[i]+ "/";
-    char buffer[PATH_MAX];
-    char* ret = getcwd(buffer,sizeof(buffer));
-    if(!ret) {
-        cerr << "Failed to get current directory... exiting" << endl;
-        return EXIT_FAILURE;
-    }
-    string currentPath(buffer);
-    const string SOURCE_DIR= currentPath+"/"+parte;
-    
-    map<string,string> options;
-    ReadConfig(nome,options);
-    
-    
-    string tmpfolder = options["bckg_path"];
-    string tmpname   = options["bckg_name"];
-    
-    if(! filesystem::exists(tmpfolder)){
-        //DEBUG
-        cout<<"Creating tmpfolder..."<<
-        system(("mkdir " + tmpfolder).c_str() );
-    }
-    
-    if(! filesystem::exists(tmpfolder+tmpname)) {
-        
-        vector<int> pedruns;
-        
-        stringstream ssruns(options["noiserun"]);
-        string run;
-        vector<string> seglist;
-        while(getline(ssruns, run, ';')) {
-            int runi = stoi(run);
-            pedruns.push_back(runi);
-        }
-        
-        cygnolib::joinMidasPedestals(pedruns, options["ped_cloud_dir"], tmpfolder, tmpname);
-        
-    }
-    
-    return 0;
-    
-}
-
-// ====== FUNCTIONS DEFINITION ======
-vector<string> split(const string& str, char delimiter)
-{
-    vector<string> tokens;
-    istringstream stream(str);
-    string token;
-
-    while(getline(stream,token,delimiter)) tokens.push_back(token);
-
-    return tokens;
-}
-void ReadConfig(string name, map<string,string>& options)
-{
-    ifstream config(name.c_str());
-    
-    string line;
-    while(getline(config,line))
-    {
-        line.erase(remove_if(line.begin(),line.end(),[] (char c){return isspace(c);}),line.end());
-        line.erase(remove_if(line.begin(),line.end(),[] (char c){return c=='\'';}),line.end());
-        if(line[0] == '#' || line.empty() || line[0] == '{' || line[0] == '}') continue;
-        
-        auto delim1= line.find(":");
-        auto delim2= line.find(",");
-        if(delim2==string::npos) delim2=line.size();
-        auto index= line.substr(0,delim1);
-        auto val= line.substr(delim1+1,min(delim2,line.size())-delim1-1 );
-        options[index]=val;
-    }
-    
-}
+ #include "ConfigManager.h"
+ #include "DigitizationRunner.h"
+ 
+ #include <iostream>
+ #include <unistd.h>
+ #include <limits.h>
+ #include <filesystem>
+ 
+ using namespace std;
+ 
+ int main(int argc, char** argv) {
+     if (argc < 2) {
+         cerr << "No Configfile given!\nUsage: ./mc_sim Configfile -I inputDir -O outputDir" << endl;
+         return EXIT_FAILURE;
+     }
+ 
+     string configFile = argv[1];
+     string inputDir, outputDir;
+ 
+     // Handle optional input/output dirs
+     if (argc == 6) {
+         string arg2 = argv[2];
+         string val2 = argv[3];
+         string arg3 = argv[4];
+         string val3 = argv[5];
+ 
+         if (arg2 == "-I" && arg3 == "-O") {
+             inputDir = val2;
+             outputDir = val3;
+         } else if (arg2 == "-O" && arg3 == "-I") {
+             outputDir = val2;
+             inputDir = val3;
+         } else {
+             cerr << "Invalid argument structure. Use -I inputDir -O outputDir" << endl;
+             return EXIT_FAILURE;
+         }
+     } else {
+         // default fallback: current working directory + relative paths
+         char cwd[PATH_MAX];
+         char* ret = getcwd(cwd, sizeof(cwd));
+         if(!ret) {
+             cerr << "Failed to get current directory... exiting" << endl;
+             return EXIT_FAILURE;
+         }
+         string base = std::filesystem::path(configFile).parent_path().string();
+         inputDir = string(cwd) + "/" + base + "/../OutDir/";
+         outputDir = string(cwd) + "/" + base + "/../OutDir/";
+     }
+ 
+     
+     
+     try {
+         DigitizationRunner runner(configFile, inputDir, outputDir);
+         runner.runPedsOnly();
+     } catch (const std::exception& e) {
+         cerr << "Error during joinPeds: " << e.what() << endl;
+         return EXIT_FAILURE;
+     }
+ 
+     return EXIT_SUCCESS;
+ }
+ 
